5ef9b537de02e4ecfa70491c3c13541a04e8a451
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2020-02-09 11:38:22 -0800

    Remove looping over queue size

--- pythoncode/graphs/open_lock.py
+++ pythoncode/graphs/open_lock.py
@@ -1,5 +1,4 @@
 from collections import deque
-import time
 class Solution(object):
     def openLock(self, deadends, target):
         """
@@ -12,25 +11,21 @@ class Solution(object):
             return -1
         q = deque()
         visited = set()
-        q.append(start)
-        visited.add(start)
-        step = 0       
+        q.append((start,0))
+        visited.add(start)      
         while len(q) != 0:
-            step += 1
-            qsize = len(q)
-            for i in xrange(qsize):
-                s = q.popleft()
-                for i in xrange(len(start)):
-                    temp1 = s[0:i] + str((int(s[i]) - 1) % 10) + s[i+1:]
-                    temp2 = s[0:i] + str((int(s[i]) + 1) % 10) + s[i+1:]
-                    for item in [temp1, temp2]:
-                        if item == target:
-                            return step
-                        elif item in visited or item in deadends:
-                            pass
-                        else:
-                            q.append(item)
-                            visited.add(item)
+            s, length = q.popleft()
+            for i in xrange(len(s)):
+                temp1 = s[0:i] + str((int(s[i]) - 1) % 10) + s[i+1:]
+                temp2 = s[0:i] + str((int(s[i]) + 1) % 10) + s[i+1:]
+                for item in [temp1, temp2]:
+                    if item == target:
+                        return length + 1
+                    elif item in visited or item in deadends:
+                        pass
+                    else:
+                        q.append((item, length +1))
+                        visited.add(item)
         return -1
 
 deadends = ["0201","0101","0102","1212","2002"] 
