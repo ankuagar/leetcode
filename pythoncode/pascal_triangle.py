463d63dda7bd94b09dffc3cfbc407f7c3ed170e0
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2021-03-13 15:59:08 -0800

    Get row in Pascal's triangle

--- pythoncode/pascal_triangle.py
+++ pythoncode/pascal_triangle.py
@@ -55,3 +55,36 @@ t.assertEqual(6, getElement(4,2))
 t.assertEqual(4, getElement(4,3))
 t.assertEqual(1, getElement(4,4))
 #print(cache)
+
+
+
+def getRow(row):
+    '''
+    Get row'th row of Pascal's triangle.
+
+    row and col are 0 indexed
+
+    row n has n+1 columns, e.g.
+    row0 has 1 column: col0
+    row1 has 2 columns: col0, col1
+    row2 has 3 columns: col0, col1, col2
+
+    item[row][col] = 1, if col = 0 or col == row
+    item[row][col] = item[row-1][col-1] + item[row-1][col]
+    '''
+    if row == 0:
+        return [1]
+    elif row == 1:
+        return [1,1]
+    elif row > 1:
+        previousRow = getRow(row - 1)
+        ans = [1,1]
+        ans[1:1] = (previousRow[i] + previousRow[i+1]  for i in range(len(previousRow)-1))
+        return ans
+
+t = unittest.TestCase()
+t.assertEqual(getRow(0), [1])
+t.assertEqual(getRow(1), [1,1])
+t.assertEqual(getRow(2), [1,2,1])
+t.assertEqual(getRow(3), [1,3,3,1])
+t.assertEqual(getRow(4), [1,4,6,4,1])
