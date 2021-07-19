3fbb3b1c241d1ee5c2a1e25c212d0e481337588f
ankuagar <ankur.a.agarwal@gmail.com>
2021-07-19 12:25:15 -0700

    Update aliases.sh
    Added git config entry commands for user.name and user.email

--- aliases.sh
+++ aliases.sh
@@ -5,3 +5,6 @@ git config --local alias.st status
 git config --local alias.br branch
 git config --local alias.ci commit
 
+git config --global user.name "Ankur Agarwal"
+git config --global user.email "ankur@email.com"
+
